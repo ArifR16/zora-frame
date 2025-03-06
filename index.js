074de17bb4a22a27bdf6b97983f444e27701a662
Arkincf4 <126714688+ArifR16@users.noreply.github.com>
2025-03-07 02:36:29 +0700

    Update and rename README.md to index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+<head>
+  <!-- Metadata Farcaster Frame -->
+  <meta property="fc:frame" content="vNext">
+  <meta property="fc:frame:image" content="https://zora.co/og-images/coin/base:0xe85908bb6d17520d358d057bba219f1668b21dde.png">
+  <meta property="fc:frame:button:1" content="View on Zora">
+  <meta property="fc:frame:button:1:action" content="link">
+  <meta property="fc:frame:button:1:target" content="https://zora.co/coin/base:0xe85908bb6d17520d358d057bba219f1668b21dde">
+  <meta property="fc:frame:post_url" content="https://zora-dex.vercel.app/api/frame">
+</head>
